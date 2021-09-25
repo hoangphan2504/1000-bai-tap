bdbd4a555a2d0290acb69faae774ab4541873d80
hoangphan2504 <87514379+hoangphan2504@users.noreply.github.com>
2021-09-25 09:58:56 +0700

    Create 450.cpp

---
+++ 450.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void NhapMang(int arr[][100], int &n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+}
+
+void XuatMang(int arr[][100], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int DemCapGiaTriDoiXungQuaDuongCheoChinh(int arr[][100], int n)
+{
+    int dem = 0;
+    for(int i = 1; i < n; i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            if(arr[i][j] == arr[j][i])
+            {
+                dem++;
+            }
+        }
+    }
+    return dem;
+}
+
+int main()
+{
+  int n;
+  cout <<"Nhap so: ";
+  cin >> n;
+  int arr[100][100];
+  cout <<"Nhap mang: ";
+  NhapMang(arr, n);
+  XuatMang(arr, n);
+  cout << DemCapGiaTriDoiXungQuaDuongCheoChinh(arr, n);
+}
